d97d15946658d4d13cd3fd4d904ff4a648218ccb
first <firsthanaa@gmail.com>
2018-05-31 15:30:30 +0700

    validation form and length of hash

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,13 +47,24 @@ class HomeController extends Controller
 
     public function save(Request $request)
     {
+        $request->validate([
+            'algorithm' => 'required',
+            'min_length' => 'required',
+            'max_length' => 'required',
+            'hash' => 'required'
+        ]);
         $job = new Job();
         $job->user_id = Auth::user()->id;
         $job->algorithm = $request->input('algorithm');
         $job->min_length = $request->input('min_length');
         $job->max_length = $request->input('max_length');
-        $job->save();
         $array_hash =  explode("\r\n", $request->input('hash'));
+        for($i = 0; $i < count($array_hash); $i++ ){
+            if ($array_hash[$i] < $request->input('min_length') || $array_hash[$i] > $request->input('max_length')){
+                return back()->with('error', 'Please check length of hash');
+            }
+        }
+        $job->save();
         for($i = 0; $i < count($array_hash); $i++){
             $job_Result = new JobResult();
             $job_Result->job_id = Job::all()->last()->id;
@@ -61,7 +72,6 @@ class HomeController extends Controller
             $job_Result->plain = '';
             $job_Result->save();
         }
-
         //--------------------------------------refresh job page------------------------------------------------//
         $idUser = Auth::user()->id;
         $listJob = Job::where('user_id', $idUser)->get();
@@ -75,7 +85,6 @@ class HomeController extends Controller
     {
         JobResult::where('job_id', $job_id)->delete();
         Job::where('id', $job_id)->delete();
-
         //--------------------------------------refresh job page------------------------------------------------//
         $idUser = Auth::user()->id;
         $listJob = Job::where('user_id', $idUser)->get();
--- resources/views/add-job.blade.php
+++ resources/views/add-job.blade.php
@@ -13,65 +13,80 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                            <form action="{{route('save-job')}}" method="POST">
-                                {{ csrf_field() }}
-                                <div class="row">
-                                    <div class="col-sm-3">Algorithm</div>
-                                    <div class="col-sm-6 input-group mb-3">
-                                        <select class="custom-select" name="algorithm">
-                                            <option selected>Choose...</option>
-                                            <option value="md5">MD5</option>
-                                            <option value="sha1">Sha1</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-sm"></div>
-                                </div>
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                <ul>
+                                    <li> {!! session()->get('error') !!}</li>
+                                </ul>
+                            </div>
+                        @endif
 
-                                <div class="row">
-                                    <div class="col-sm-3">Plain Text Range</div>
-                                    <div class="col-sm-3 input-group mb-3">
-                                        <select class="custom-select" name="min_length">
-                                            <option selected>Choose...</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                            <option value="11">11</option>
-                                            <option value="12">12</option>
-                                        </select>
-                                    </div>
-                                    to
-                                    <div class="col-sm-3">
-                                        <select class="custom-select" name="max_length">
-                                            <option selected>Choose...</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                            <option value="11">11</option>
-                                            <option value="12">12</option>
-                                        </select>
-                                    </div>
-                                </div>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
-                                <div class="row">
-                                    <div class="col-sm-3">Hash<br>(1 hash per line)</div>
-                                    <div class="col-sm-8 input-group mb-3">
-                                        <textarea class="form-control" name="hash" rows="3"></textarea>
-                                    </div>
-                                    <div class="col-sm"></div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-3"></div>
-                                    <div class="col-sm-8">
-                                        <button type="submit" class="btn btn-success" style="width: 100px">Submit</button>
-                                    </div>
-                                    <div class="col-sm"></div>
-                                </div>
-                            </form>
+                        <form action="{{route('save-job')}}" method="POST">
+                            {{ csrf_field() }}
 
+                            <div class="row">
+                                <div class="col-sm-3">Algorithm</div>
+                                <div class="col-sm-6 input-group mb-3">
+                                    <select class="custom-select" name="algorithm">
+                                        <option value="md5">MD5</option>
+                                        <option value="sha1">Sha1</option>
+                                    </select>
+                                </div>
+                                <div class="col-sm"></div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-3">Plain Text Range</div>
+                                <div class="col-sm-3 input-group mb-3">
+                                    <select class="custom-select" name="min_length">
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                    </select>
+                                </div>
+                                to
+                                <div class="col-sm-3">
+                                    <select class="custom-select" name="max_length">
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-sm-3">Hash<br>(1 hash per line)</div>
+                                <div class="col-sm-8 input-group mb-3">
+                                    <textarea class="form-control" name="hash" rows="3"></textarea>
+                                </div>
+                                <div class="col-sm"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-8">
+                                    <button type="submit" class="btn btn-success" style="width: 100px">Submit</button>
+                                </div>
+                                <div class="col-sm"></div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
