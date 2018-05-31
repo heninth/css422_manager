cccf53b221cf1f43cc856a5ccc828433eaca517e
Temwalker <sutepnopnatee@gmail.com>
2018-06-01 02:41:29 +0700

    getTask Api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Worker;
+use App\Job;
+use App\JobResult;
+use App\Task;
 
 class ApiController extends Controller
 {
@@ -55,7 +58,61 @@ class ApiController extends Controller
         return json_encode(['success' => true]);
     }
 
-    public function getTask(Request $request){
+    public function getTask(){
+      $jobs = Job::where('status','running')->get(); // look in the job that running
+      $newTask = false;
+      foreach ($jobs as $job) {
+        $flag = 0; //check if we got task in sub loop already ?
+        $tasks = Task::where([['status','!=','finished'],['job_id',$job->id]])->get();
+        foreach ($tasks as $task) {//check all the task that running or queue if have some of them already timeout let get it start again
+            if($task->status == 'running'){
+              if($task->timeout_at < Carbon::now('Asia/Bangkok') ){
+                $newTask = true;
+                $taskId = $task->id;
+                $algo = $job->algorithm;
+                $start = $task->start_hash;
+                $end = $task->end_hash;
+                $range = $task->range;
+                $timeout = 120;
+                $hashes = JobResult::select('hash')->where('job_id',$job->id)->get();
+                $flag = 1;
+                $task->timeout_at = Carbon::now('Asia/Bangkok')->addMinutes(2);
+                $task->save();
+                break;
+              }
+            }
+            elseif($task->status == 'queue') {
+              $newTask = true;
+              $taskId = $task->id;
+              $algo = $job->algorithm;
+              $start = $task->start_hash;
+              $end = $task->end_hash;
+              $range = $task->range;
+              $timeout = 120;
+              $hashes = JobResult::select('hash')->where('job_id',$job->id)->get();
+              $flag = 1;
+              $task->timeout_at = Carbon::now('Asia/Bangkok')->addMinutes(2);
+              $task->status = 'running';
+              $task->save();
+              break;
+            }
+        }
+        if($flag == 1){
+            break;
+        }//inner loop
+      }//1stloop
+
+    return json_encode([
+                      'newTask' => $newTask ,
+                      'taskId' => $taskId ,
+                      'algo' => $algo ,
+                      'start' => $start ,
+                      'end' => $end ,
+                      'range' => $range ,
+                      'timeout' => $timeout ,
+                      'hashes' => $hashes ,
+                      ]);
 
     }
+
 }
